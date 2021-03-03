80015f70a51fb210e7647d67cfc94dd198d1cb52
n_herr03 <n_herr03@uni-muenster.de>
2021-03-03 17:39:06 +0100

    local changes

--- MusketProgrammwithoutfileread/Knapsack/CUDA/build-and-run.sh
+++ MusketProgrammwithoutfileread/Knapsack/CUDA/build-and-run.sh
@@ -1,18 +1,18 @@
 #!/bin/bash
 
 # remove files and create folder
-rm -rf -- build && \
-mkdir build && \
+#rm -rf -- build && \
+#mkdir build && \
 
 # run cmake
-cd build && \
-cmake -G "Unix Makefiles" -D CMAKE_BUILD_TYPE=Dev -D CMAKE_CXX_COMPILER=g++ ../ && \
+#cd build && \
+#cmake -G "Unix Makefiles" -D CMAKE_BUILD_TYPE=Dev -D CMAKE_CXX_COMPILER=g++ ../ && \
 
-make Knapsack_0 && \
+#make Knapsack_0 && \
 echo "run; iterations; problem; colony size; Fitness; Total Time "
 for KNAP in 1 3 4 5 6 7 
 do
-	for RUN in 1
+	for RUN in 2
 	do
 		bin/Knapsack_0 $RUN 10 $KNAP
 	done	
