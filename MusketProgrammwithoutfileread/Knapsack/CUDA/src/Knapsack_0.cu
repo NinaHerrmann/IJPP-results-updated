eb5569d0fefe8afd3687f6e44d04ed9bc1b9e212
n_herr03 <n_herr03@uni-muenster.de>
2021-03-11 17:20:37 +0100

    try

--- MusketProgrammwithoutfileread/Knapsack/CUDA/src/Knapsack_0.cu
+++ MusketProgrammwithoutfileread/Knapsack/CUDA/src/Knapsack_0.cu
@@ -149,22 +149,20 @@
                         double set = ((d_eta.get_global((((ant_index) * (d_n_objects)) + (object_j))) * d_tau.get_global((((ant_index) * (d_n_objects)) + (object_j))))  / (eta_tau_sum));
                         d_probabilities.set_global((((ant_index) * (d_n_objects)) + (object_j)), set);
                     }*/
-                    //double random =  curand_uniform(&d_rand_states_ind[ant_index]);
-                    double random = 0.1;
+                    double random =  curand_uniform(&d_rand_states_ind[ant_index]);
                     select_index = 0;
                     int selected_object = 0;
                     double sum = 0.0;
                     double prob = 0.0;
-                    /*while ((sum <= random) && (select_index < d_n_objects)){
-                        //prob = d_probabilities.get_global((((ant_index) * (d_n_objects)) + (select_index)));
+                    while ((sum <= random) && (select_index < d_n_objects)){
+                        prob = d_probabilities.get_global((((ant_index) * (d_n_objects)) + (select_index)));
 
                         if(((prob) > 0.0)){
                             sum = ((sum) + (prob));
                             selected_object = (select_index);
                         }
                         select_index = ((select_index) + 1);
-                    }*/
-                    selected_object = 1;
+                    }
                     //d_ant_solutions.set_global((((ant_index) * (d_n_objects)) + (step)), (selected_object));
 
                     //d_ant_available_objects.set_global((((ant_index) * (d_n_objects)) + (selected_object)), 0);
