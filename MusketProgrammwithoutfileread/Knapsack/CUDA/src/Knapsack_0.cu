b82e41ce04b19acbbafd5f62caf39ecfd62570c5
n_herr03 <n_herr03@uni-muenster.de>
2021-03-15 18:25:58 +0100

    local path

--- MusketProgrammwithoutfileread/Knapsack/CUDA/src/Knapsack_0.cu
+++ MusketProgrammwithoutfileread/Knapsack/CUDA/src/Knapsack_0.cu
@@ -518,7 +518,7 @@ __global__ void mkt::kernel::reduce_max(int *g_idata, int *g_odata, unsigned int
                                                         mkt::COPY);
 
                 std::string file_name = "mknap1";
-		std::string basepath = "/root/IJPP-results-updated/MusketProgrammwithoutfileread";
+		std::string basepath = "/home/schredder/research/IJPP/IJPP-results-updated/MusketProgrammwithoutfileread";
                 switch (problem) {
                     case 1:
                         file_name = basepath + "/Knapsack/CUDA/mknap1";
